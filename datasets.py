81f8c7d9096a00ae1d42dc4d2242cb10fa025dee
Darshan Patil <dapatil211@gmail.com>
2021-04-20 16:20:33 -0400

    Added datasets skeleton file

---
+++ datasets.py
@@ -0,0 +1,19 @@
+
+def create_split_cifar100():
+    pass
+
+def create_split_mnist():
+    pass
+
+def create_permuted_mnist():
+    pass
+
+def get_dataset(name):
+    if name == 'split_cifar100':
+        return create_split_cifar100()
+    elif name == 'split_mnist':
+        return create_split_mnist()
+    elif name == 'permuted_mnist':
+        return create_permuted_mnist()
+    
+    raise ValueError(f'Dataset {name} is not available')
