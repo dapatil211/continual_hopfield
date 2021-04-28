d5e41b84b2c41d0c9f4d56e4e5ace3c6fbf204cc
Ali Rahimi-Kalahroudi <alirahkay@gmail.com>
2021-04-28 00:00:04 -0400

    Init deep generative replay

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torchvision
+from torch import optim
+import copy
+from generative_replay import WGAN
 
 
 def get_model(args):
@@ -33,11 +36,11 @@ class ListBuffer(nn.Module):
         num_to_add = 0
         if self.num_added < self.buffer_size:
             num_to_add = min(self.buffer_size - self.num_added, batch_size)
-            self.X[self.num_added : self.num_added + num_to_add] = X[:num_to_add]
-            self.y[self.num_added : self.num_added + num_to_add] = y[:num_to_add]
-            self.task_ids[self.num_added : self.num_added + num_to_add] = task_ids[
-                :num_to_add
-            ]
+            self.X[self.num_added: self.num_added + num_to_add] = X[:num_to_add]
+            self.y[self.num_added: self.num_added + num_to_add] = y[:num_to_add]
+            self.task_ids[self.num_added: self.num_added + num_to_add] = task_ids[
+                                                                         :num_to_add
+                                                                         ]
             self.num_added += num_to_add
             self.num_viewed += num_to_add
         if num_to_add < batch_size:
@@ -48,7 +51,7 @@ class ListBuffer(nn.Module):
     def _maybe_replace_in_buffer(self, X, y, task_ids):
         batch_size = X.size(0)
         inds = (
-            torch.rand(batch_size) * (torch.arange(batch_size) + self.num_viewed + 1)
+                torch.rand(batch_size) * (torch.arange(batch_size) + self.num_viewed + 1)
         ).long()
         replace_locs = inds < self.buffer_size
         X = X[replace_locs]
@@ -86,3 +89,54 @@ class TinyEpisodicMemoryModel(BaseModel):
 
         self.buffer.add_to_buffer(X, y, task_ids)
 
+
+class GenerativeReplay(BaseModel):
+    def __init__(self, logit_masks, image_size, image_channel_size,
+                 critic_channel_size, generator_channel_size, num_classes=100):
+        super().__init__()
+        self.task_logit_masks = logit_masks
+
+        self.base = torchvision.models.resnet18(num_classes=num_classes)
+        self.gen = WGAN(image_size, image_channel_size, critic_channel_size, generator_channel_size)
+
+        gen_g_optimizer = optim.Adam(
+            self.gen.generator.parameters(),
+            lr=1e-3, weight_decay=1e-5, betas=(.5, .9),
+        )
+        gen_c_optimizer = optim.Adam(
+            self.gen.critic.parameters(),
+            lr=1e-3, weight_decay=1e-5, betas=(.5, .9),
+        )
+        self.gen.set_lambda(10.)
+        self.gen.set_generator_optimizer(gen_g_optimizer)
+        self.gen.set_critic_optimizer(gen_c_optimizer)
+        self.gen.set_critic_updates_per_generator_update(5)
+
+        self.prev_base = None
+        self.prev_gen = None
+
+    def get_loss(self, X, y):
+        batch_size = X.shape[0]
+        er_X, er_y = None, None
+        if self.prev_gen is not None:
+            er_X, er_y = self.sample(batch_size)
+            full_batch_X = torch.cat((X, er_X))
+            full_batch_y = torch.cat((y, er_y))
+        else:
+            full_batch_X = X
+            full_batch_y = y
+        logits = self.base(full_batch_X)
+
+        self.gen.train_a_batch(X, y, er_X, er_y)
+
+    def switch_task(self):
+        self.prev_base = copy.deepcopy(self.base)
+        self.prev_gen = copy.deepcopy(self.gen)
+
+    def sample(self, size):
+        if self.prev_gen is None:
+            return
+        x = self.prev_gen.sample(size)
+        prev_scores = self.prev_base(x)
+        _, y = torch.max(prev_scores, 1)
+        return x.data, y.data
